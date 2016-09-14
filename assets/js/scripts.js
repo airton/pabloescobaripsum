79840f1f012ceab153ceb25270e00186dc377ae5
airton <airtonvancin@gmail.com>
2016-09-14 10:24:31 -0300

    Add Quotes

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -32,7 +32,9 @@
               "Ustedes pueden aceptar mi negocio o aceptar las consequências. ¿Plata o plomo? Compremos una lavadora más grande, pues. Los políticos se espantan fácil. Una pistola y ya. La cárcel em Estados Unidos es peor que la muerte.",
               "Ya no queda nadie que se nos enfrente. ¿Y sabes por qué, Fabito? Porque nadie tiene los huevos como nosotros. A la guerra, pues. Las mentiras son necesarias cuando la verdade es muy difícil de creer. El propósito de la guerra es la paz. Hay momentos para pelear y hay momentos para ser astutos.",
               "¿Plata o plomo? Ustedes pueden aceptar mi negocio o aceptar las consequências. Una pistola y ya. A la guerra, pues. Las mentiras son necesarias cuando la verdade es muy difícil de creer.",
-              "Soy el fuego que arde tu piel. Soy el agua que mata tu sed. El castillo, la torre yo soy. La espada que guarda el caudal. Tu el aire que respiro yo. Y la luz de la luna en el mar. La garganta que ansio mojar. Que temo ahogar de amor. Y cuales deseos me vas a dar? Dices tu: mi tesoro basta con mirarlo. Y tuyo será, y tuyo será"
+              "Soy el fuego que arde tu piel. Soy el agua que mata tu sed. El castillo, la torre yo soy. La espada que guarda el caudal. Tu el aire que respiro yo. Y la luz de la luna en el mar. La garganta que ansio mojar. Que temo ahogar de amor. Y cuales deseos me vas a dar? Dices tu: mi tesoro basta con mirarlo. Y tuyo será, y tuyo será",
+              "Dígame una cosa, señor Escobar. ¿Ni siquiera es capaz de esconder su mercancía, su contrabando? Yo pago por el privilegio, coronel. ¿Ah sí? No me diga. Les voy a decir quién soy. Yo soy Pablo Emilio Escobar Gaviria. Usted es el coronel José Luis Herrera. ¿Usted cómo sabe mi nombre? Mis ojos están en todos lados.",
+              "Sí, señores. No pueden mover un dedo. Ustedes pueden aceptar mi negocio o aceptar las consecuencias. ¿Plata o plomo? Ustedes eligen. Yo soy Pablo Emilio Escobar Gaviria."
             ];
 
             app.number.addEventListener('change', function(e) {
